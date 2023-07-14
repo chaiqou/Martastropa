c5af0589827fdbab906963ea6e6ffecf09e6cf54
chaiqou <lomtadzenikusha@gmail.com>
2023-07-14 13:47:55 +0400

    chore: fix navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,11 @@ const Navbar = () => {
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100  z-10 py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href="/" className="flex gap-2 items-center">
-          <p className="hidden text-zinc-700 text-sm font-black md:block">
+          <p className="hidden text-pink-600 text-sm font-black md:block">
             Martastropa
           </p>
           <HeartPulse
-            color="#be2323"
+            color="#eb0089"
             absoluteStrokeWidth
             className="h-8 w-8 sm:h-6 sm:w-6 "
           />
