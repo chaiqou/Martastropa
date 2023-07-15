3f3d6a12616dc0b226f0ca1e6135ca0613b288ad
chaiqou <lomtadzenikusha@gmail.com>
2023-07-15 23:36:31 +0400

    feat: handle google authentication with next auth and Prisma

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { HeartPulse } from "lucide-react";
 import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "../lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getAuthSession();
+  console.log(session);
+
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100  z-10 py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -17,9 +21,13 @@ const Navbar = () => {
           />
         </Link>
 
-        <Link className={buttonVariants()} href="/sign-in">
-          Sign in
-        </Link>
+        {session ? (
+          <p>You are logged in</p>
+        ) : (
+          <Link className={buttonVariants()} href="/sign-in">
+            Sign in
+          </Link>
+        )}
       </div>
     </div>
   );
