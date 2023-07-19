09c7d9f3de95afe853550633f9136e5ba4db0b70
chaiqou <lomtadzenikusha@gmail.com>
2023-07-19 14:21:56 +0400

    fix: next auth config also add avatar and dropdown components

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,20 @@
 import type { Session, User } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 
-type userId = string;
+type UserId = string;
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: userId;
+    id: UserId;
     username?: string | null;
   }
 }
 
 declare module "next-auth" {
   interface Session {
-    id: userId;
-    username?: string | null;
+    user: User & {
+      id: UserId;
+      username?: string | null;
+    };
   }
 }
