0cbd4b0a4a3206920827d4e16f7abf6ae8490250
chaiqou <lomtadzenikusha@gmail.com>
2023-07-20 19:41:02 +0400

    feat: Display user avatar

--- components/UserAccountNav.tsx
+++ components/UserAccountNav.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { FC } from "react";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
 } from "../components/ui/DropdownMenu";
 import UserAvatar from "./UserAvatar";
+import { User } from "next-auth";
 
-const UserAccountNav = ({ user }) => {
+interface UserAccountNavProps {
+  user: Pick<User, "name" | "email" | "image">;
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar user={user} />
+        <UserAvatar
+          className="h-8 w-8"
+          user={{ name: user.name || null, image: user.image || null }}
+        ></UserAvatar>
       </DropdownMenuTrigger>
     </DropdownMenu>
   );
