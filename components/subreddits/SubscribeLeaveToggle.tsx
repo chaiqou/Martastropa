45fa262f7ffd8aea9cc0f146985d28ce10a4ba18
chaiqou <lomtadzenikusha@gmail.com>
2023-08-10 22:18:10 +0400

    feat: Handle unsubscribe logic for community

--- components/subreddits/SubscribeLeaveToggle.tsx
+++ components/subreddits/SubscribeLeaveToggle.tsx
@@ -25,7 +25,7 @@ const SubscribeLeaveToggle = ({
 
   const { mutate: subscribe, isLoading: isSubLoading } = useMutation({
     mutationFn: async () => {
-      const payload: SubscribeToSubredditPayload = {
+      const payload: typeof SubscribeToSubredditPayload = {
         subredditId,
       }
 
@@ -58,11 +58,51 @@ const SubscribeLeaveToggle = ({
     },
   })
 
+  const { mutate: unsubscribe, isLoading: isUnsubscribeLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: typeof SubscribeToSubredditPayload = {
+        subredditId,
+      }
+
+      const { data } = await axios.post('/api/subreddit/unsubscribe', payload)
+      return data as string
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast()
+        }
+      }
+
+      return toast({
+        title: 'There was a problem.',
+        description: 'Something went wrong. Please try again.',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        // Refresh the current route and fetch new data from the server without
+        // losing client-side browser or React state.
+        router.refresh()
+      })
+      toast({
+        title: 'Unsubscribed!',
+        description: `You are now unsubscribed from r/${subredditName}`,
+      })
+    },
+  })
+
+
+
   
 
   return isSubscribed ? (
     <Button
-      className='w-full mt-1 mb-4'>
+      className='w-full mt-1 mb-4'
+      isLoading={isUnsubscribeLoading}
+      onClick={() => unsubscribe()}
+      >
       Leave community
     </Button>
   ) : (
