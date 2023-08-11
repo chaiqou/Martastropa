4ea4256442602282c60aa9def03e7f397be3597c
chaiqou <lomtadzenikusha@gmail.com>
2023-08-11 15:50:43 +0400

    wip: Handle form submiting auto validation with zod and react hook form

--- app/m/[slug]/submit/page.tsx
+++ app/m/[slug]/submit/page.tsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import { db } from '../../../../lib/db'
-import { notFound } from 'next/navigation'
-import { Button } from '../../../../components/ui/Button'
-import Editor from '../../../../components/subreddits/submit-post/Editor'
+import React from "react";
+import { db } from "../../../../lib/db";
+import { notFound } from "next/navigation";
+import { Button } from "../../../../components/ui/Button";
+import Editor from "../../../../components/subreddits/submit-post/Editor";
 
 interface PageProps {
-    params: {
-        slug: string,
-    }
+  params: {
+    slug: string;
+  };
 }
 
-const page = async ({params}: PageProps) => {
+const page = async ({ params }: PageProps) => {
   const subreddit = db.subreddit.findFirst({
     where: {
-        name: params.slug,
-    }
-  })
+      name: params.slug,
+    },
+  });
 
-  if(!subreddit) return notFound();
-
+  if (!subreddit) return notFound();
 
   return (
-    <div className='flex flex-col items-start gap-6'>
-        <div className='border-b border-gray-200 pb-5'>
-            <div className='-ml-2 -mt-2 flex flex-wrap items-baseline'>
-                <h3 className='ml-2 mt-3 text-base font-semibold leading-6 text-gray-900'>Create Post</h3>
-                <p className='ml-2 mt-1 truncate text-sm text-gray-500'>In m/{params.slug}</p>
-            </div>
+    <div className="flex flex-col items-start gap-6">
+      <div className="border-b border-gray-200 pb-5">
+        <div className="-ml-2 -mt-2 flex flex-wrap items-baseline">
+          <h3 className="ml-2 mt-3 text-base font-semibold leading-6 text-gray-900">
+            Create Post
+          </h3>
+          <p className="ml-2 mt-1 truncate text-sm text-gray-500">
+            In m/{params.slug}
+          </p>
         </div>
+      </div>
 
-        <Editor />
-        <div className='w-full flex justify-end'>
-            <Button form='subreddit-post-form' className='w-full' type='submit'>Post</Button>
-        </div>
+      <Editor subredditId={subreddit.id} />
+      <div className="w-full flex justify-end">
+        <Button form="subreddit-post-form" className="w-full" type="submit">
+          Post
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- components/subreddits/submit-post/Editor.tsx
+++ components/subreddits/submit-post/Editor.tsx
@@ -1,16 +1,42 @@
-'use client';
-import TextareaAutosize from 'react-textarea-autosize';
+"use client";
+import TextareaAutosize from "react-textarea-autosize";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  PostValidator,
+  PostCreationRequest,
+} from "../../../lib/validators/post";
 
-const Editor = () => {
-  return (
-    <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
-        <form className='w-fit ' id='subreddit-post-form'>
-            <div className='prose prose-stone dark:prose-invert'>
-                <TextareaAutosize placeholder='Title' className='w-full resize-none overflow-hidden appearance-none font-bold outline-none bg-transparent text-5xl' />
-            </div>
-        </form>
-    </div>
-  )
+interface editorProps {
+  subredditId: string;
 }
 
-export default Editor
+const Editor = ({ subredditId }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(PostValidator),
+    defaultValues: {
+      subredditId,
+      title: "",
+      content: null,
+    },
+  });
+
+  return (
+    <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
+      <form className="w-fit " id="subreddit-post-form">
+        <div className="prose prose-stone dark:prose-invert">
+          <TextareaAutosize
+            placeholder="Title"
+            className="w-full resize-none overflow-hidden appearance-none font-bold outline-none bg-transparent text-5xl"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Editor;
